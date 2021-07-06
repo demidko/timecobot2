b01d82a85d69d632ef75d5263da04ea64dad546b
Daniil Demidko <demidko@farpost.com>
2021-07-07 00:29:31 +1000

    build script update

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <tgbot/tgbot.h>
+#include <fmt/core.h>
 
 int main(int argc, char **argv) {
   auto bot = TgBot::Bot(getenv("TOKEN"));
   bot.getEvents().onAnyMessage([api = bot.getApi()](const TgBot::Message::Ptr &m) {
-    if (auto sourceMessage = m->replyToMessage; sourceMessage) {
-      if (auto sender = sourceMessage->from; sender) {
-        api.sendMessage(m->chat->id, "It depends", false, sender->id);
-      }
+    if (!m->replyToMessage) {
+      fmt::print("no-reply: {}\n", m->text);
+      return;
     }
+    if (!m->replyToMessage->from) {
+      fmt::print("no-reply-sender: {}\n", m->text);
+    }
+    api.sendMessage(m->chat->id, "It works", false, m->replyToMessage->from->id);
   });
   auto longPoll = TgBot::TgLongPoll(bot);
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
   while (true) {
+    fmt::print("Updated\n");
     longPoll.start();
   }
 #pragma clang diagnostic pop
-
 }
