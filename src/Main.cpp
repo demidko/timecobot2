9e122ef081846096fd93efc43b0b81b55e301804
Daniil Demidko <demidko@farpost.com>
2021-07-06 23:43:49 +1000

    proof of conception

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <tgbot/tgbot.h>
 
 int main(int argc, char **argv) {
-  std::cout << "It works" << std::endl;
+
+  TgBot::Bot bot(getenv("TOKEN"));
+  std::cout << bot.getApi().getMe()->username;
+  /*bot.getEvents().onAnyMessage([&bot](const TgBot::Message::Ptr &message) {
+
+  });
+  TgBot::TgLongPoll(bot).start();*/
 }
